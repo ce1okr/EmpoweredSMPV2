72ce7d13cc9b1c82ba11ef377c5ad96f0a2b4397
ce1okr <josh.hyunjin@gmail.com>
2026-09-19 00:55:43 -0400

    Add WorldLockManager and listener to EmpowerSMP

--- src/main/java/com/empowersmp/EmpowerSMP.java
+++ src/main/java/com/empowersmp/EmpowerSMP.java
@@ -3,19 +3,24 @@ package com.empowersmp;
 import com.empowersmp.commands.EmpowerCommand;
 import com.empowersmp.data.DataManager;
 import com.empowersmp.listeners.PlayerConnectionListener;
+import com.empowersmp.listeners.WorldLockListener;
+import com.empowersmp.world.WorldLockManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EmpowerSMP extends JavaPlugin {
 
     private DataManager dataManager;
+    private WorldLockManager worldLockManager;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         this.dataManager = new DataManager(this);
+        this.worldLockManager = new WorldLockManager(this);
 
         getServer().getPluginManager().registerEvents(new PlayerConnectionListener(this), this);
+        getServer().getPluginManager().registerEvents(new WorldLockListener(this), this);
 
         EmpowerCommand cmd = new EmpowerCommand(this);
         getCommand("empower").setExecutor(cmd);
@@ -37,5 +42,9 @@ public class EmpowerSMP extends JavaPlugin {
 
     public DataManager getDataManager() {
         return dataManager;
+    }
+
+    public WorldLockManager getWorldLockManager() {
+        return worldLockManager;
     }
 }
