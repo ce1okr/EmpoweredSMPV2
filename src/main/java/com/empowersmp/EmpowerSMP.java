b340e57367a161b61973806496f504caedb6163e
ce1okr <josh.hyunjin@gmail.com>
2026-09-19 11:06:51 -0400

    Integrate NetheriteBanListener into EmpowerSMP
    Added NetheriteBanListener to manage netherite bans and updated periodic sweep tasks.

--- src/main/java/com/empowersmp/EmpowerSMP.java
+++ src/main/java/com/empowersmp/EmpowerSMP.java
@@ -3,6 +3,7 @@ package com.empowersmp;
 import com.empowersmp.commands.EmpowerCommand;
 import com.empowersmp.data.DataManager;
 import com.empowersmp.listeners.EnchantCapListener;
+import com.empowersmp.listeners.NetheriteBanListener;
 import com.empowersmp.listeners.PlayerConnectionListener;
 import com.empowersmp.listeners.WorldLockListener;
 import com.empowersmp.world.WorldLockManager;
@@ -14,6 +15,7 @@ public class EmpowerSMP extends JavaPlugin {
     private DataManager dataManager;
     private WorldLockManager worldLockManager;
     private EnchantCapListener enchantCapListener;
+    private NetheriteBanListener netheriteBanListener;
 
     @Override
     public void onEnable() {
@@ -21,10 +23,12 @@ public class EmpowerSMP extends JavaPlugin {
         this.dataManager = new DataManager(this);
         this.worldLockManager = new WorldLockManager(this);
         this.enchantCapListener = new EnchantCapListener(this);
+        this.netheriteBanListener = new NetheriteBanListener(this);
 
         getServer().getPluginManager().registerEvents(new PlayerConnectionListener(this), this);
         getServer().getPluginManager().registerEvents(new WorldLockListener(this), this);
         getServer().getPluginManager().registerEvents(enchantCapListener, this);
+        getServer().getPluginManager().registerEvents(netheriteBanListener, this);
 
         EmpowerCommand cmd = new EmpowerCommand(this);
         getCommand("empower").setExecutor(cmd);
@@ -33,10 +37,14 @@ public class EmpowerSMP extends JavaPlugin {
         long autosaveTicks = 5L * 60L * 20L;
         Bukkit.getScheduler().runTaskTimer(this, () -> dataManager.saveAll(), autosaveTicks, autosaveTicks);
 
-        // Fallback re-scan every 15s to catch enchant-cap violations from sources
-        // other than anvils/enchanting tables/clicks (commands, other plugins, etc).
+        // Fallback re-scans every 15s to catch enchant-cap and netherite-ban
+        // violations from sources other than the events above (commands, other
+        // plugins, etc).
         Bukkit.getScheduler().runTaskTimer(this,
                 () -> Bukkit.getOnlinePlayers().forEach(enchantCapListener::periodicSweep),
+                300L, 300L);
+        Bukkit.getScheduler().runTaskTimer(this,
+                () -> Bukkit.getOnlinePlayers().forEach(netheriteBanListener::periodicSweep),
                 300L, 300L);
 
         getLogger().info("EmpowerSMP v2 (Phase 1) enabled.");
