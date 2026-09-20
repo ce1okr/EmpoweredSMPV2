28eb6fe12310fb6cba5a6f13e0ff058c0f84b8f9
ce1okr <josh.hyunjin@gmail.com>
2026-09-20 18:00:38 -0400

    Add ClassAbilityManager and related listeners

--- src/main/java/com/empowersmp/EmpowerSMP.java
+++ src/main/java/com/empowersmp/EmpowerSMP.java
@@ -1,14 +1,21 @@
 package com.empowersmp;
 
+import com.empowersmp.classes.ClassAbilityManager;
 import com.empowersmp.commands.EmpowerCommand;
 import com.empowersmp.data.DataManager;
 import com.empowersmp.items.CraftingRecipes;
+import com.empowersmp.listeners.AutoAbilityListener;
+import com.empowersmp.listeners.CooldownDisplayTask;
 import com.empowersmp.listeners.EnchantCapListener;
+import com.empowersmp.listeners.ExclusiveEffectListener;
+import com.empowersmp.listeners.IceBreakerListener;
 import com.empowersmp.listeners.LevelUpgraderListener;
+import com.empowersmp.listeners.MinimumEnchantListener;
 import com.empowersmp.listeners.NetheriteBanListener;
 import com.empowersmp.listeners.PlayerConnectionListener;
 import com.empowersmp.listeners.PlayerDeathEconomyListener;
 import com.empowersmp.listeners.PotionRestrictionListener;
+import com.empowersmp.listeners.RangerShotListener;
 import com.empowersmp.listeners.WorldLockListener;
 import com.empowersmp.world.WorldLockManager;
 import org.bukkit.Bukkit;
@@ -18,18 +25,26 @@ public class EmpowerSMP extends JavaPlugin {
 
     private DataManager dataManager;
     private WorldLockManager worldLockManager;
+    private ClassAbilityManager classAbilityManager;
     private EnchantCapListener enchantCapListener;
     private NetheriteBanListener netheriteBanListener;
     private PotionRestrictionListener potionRestrictionListener;
+    private MinimumEnchantListener minimumEnchantListener;
+    private IceBreakerListener iceBreakerListener;
+    private CooldownDisplayTask cooldownDisplayTask;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         this.dataManager = new DataManager(this);
         this.worldLockManager = new WorldLockManager(this);
+        this.classAbilityManager = new ClassAbilityManager(this);
         this.enchantCapListener = new EnchantCapListener(this);
         this.netheriteBanListener = new NetheriteBanListener(this);
         this.potionRestrictionListener = new PotionRestrictionListener(this);
+        this.minimumEnchantListener = new MinimumEnchantListener(this);
+        this.iceBreakerListener = new IceBreakerListener(this);
+        this.cooldownDisplayTask = new CooldownDisplayTask(this);
 
         CraftingRecipes.registerAll(this);
 
@@ -38,6 +53,10 @@ public class EmpowerSMP extends JavaPlugin {
         getServer().getPluginManager().registerEvents(enchantCapListener, this);
         getServer().getPluginManager().registerEvents(netheriteBanListener, this);
         getServer().getPluginManager().registerEvents(potionRestrictionListener, this);
+        getServer().getPluginManager().registerEvents(minimumEnchantListener, this);
+        getServer().getPluginManager().registerEvents(new ExclusiveEffectListener(this), this);
+        getServer().getPluginManager().registerEvents(new AutoAbilityListener(this), this);
+        getServer().getPluginManager().registerEvents(new RangerShotListener(this), this);
         getServer().getPluginManager().registerEvents(new PlayerDeathEconomyListener(this), this);
         getServer().getPluginManager().registerEvents(new LevelUpgraderListener(this), this);
 
@@ -48,7 +67,7 @@ public class EmpowerSMP extends JavaPlugin {
         long autosaveTicks = 5L * 60L * 20L;
         Bukkit.getScheduler().runTaskTimer(this, () -> dataManager.saveAll(), autosaveTicks, autosaveTicks);
 
-        // Fallback re-scans every 15s to catch violations from sources other than
+        // Fallback re-scans every 15s to catch violations/floors from sources other than
         // the events above (commands, other plugins, etc).
         Bukkit.getScheduler().runTaskTimer(this,
                 () -> Bukkit.getOnlinePlayers().forEach(enchantCapListener::periodicSweep),
@@ -59,8 +78,20 @@ public class EmpowerSMP extends JavaPlugin {
         Bukkit.getScheduler().runTaskTimer(this,
                 () -> Bukkit.getOnlinePlayers().forEach(potionRestrictionListener::periodicSweep),
                 300L, 300L);
+        Bukkit.getScheduler().runTaskTimer(this,
+                () -> Bukkit.getOnlinePlayers().forEach(minimumEnchantListener::periodicSweep),
+                300L, 300L);
 
-        getLogger().info("EmpowerSMP v2 enabled.");
+        // Once-a-second tasks: Ice Breaker's sprint-on-ice tracking and the small
+        // cooldown action-bar timer for the 4 auto-trigger abilities.
+        Bukkit.getScheduler().runTaskTimer(this,
+                () -> Bukkit.getOnlinePlayers().forEach(iceBreakerListener::tick),
+                20L, 20L);
+        Bukkit.getScheduler().runTaskTimer(this,
+                () -> Bukkit.getOnlinePlayers().forEach(cooldownDisplayTask::tick),
+                20L, 20L);
+
+        getLogger().info("EmpowerSMP v2 (Phase 2 - abilities) enabled.");
     }
 
     @Override
@@ -77,5 +108,9 @@ public class EmpowerSMP extends JavaPlugin {
 
     public WorldLockManager getWorldLockManager() {
         return worldLockManager;
+    }
+
+    public ClassAbilityManager getClassAbilityManager() {
+        return classAbilityManager;
     }
 }
